61ea158fadf81809cc57c87109d49f6e2fda9071
Chouayeb Ben ltaief <77885857+pirate444@users.noreply.github.com>
2025-07-25 07:19:15 +0100

    access mode for activities files

--- backend/utils/cloudinaryStorage.js
+++ backend/utils/cloudinaryStorage.js
@@ -1,11 +1,20 @@
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('./cloudinary');
 
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'uploads', // Optional: change folder name as needed
     allowed_formats: ['jpg', 'png', 'mp4', 'pdf', 'docx', 'jpeg', 'gif'], // Add formats you need
+    resource_type: 'auto',
+    // 👈 This makes sure all uploaded files are private
+    access_mode: 'authenticated'
   },
 });
 
